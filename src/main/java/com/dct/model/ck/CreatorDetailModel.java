27bf8a91b00965f4f8266a91d6e2650649d5c52a
david.li <david.li@starpavilion-digital.com>
2024-09-25 18:50:12 +0800

    更新

---
+++ src/main/java/com/dct/model/ck/CreatorDetailModel.java
@@ -0,0 +1,48 @@
+package com.dct.model.ck;/**
+ * @program dct
+ * @description:
+ * @author: lichen
+ * @create: 2024/09/25 15:55
+ */
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @program dct
+ * @description:
+ * @author: lichen
+ * @create: 2024/09/25 15:55 
+ */
+@Entity
+@Table(name = "video_detail")
+@Data
+public class CreatorDetailModel extends {
+
+    @Id
+    @Column(name = "id")
+    public String id;
+
+    /**
+     * 账号(starp/vista)
+     */
+    @Column(name = "account")
+    public String account;
+
+    @Column(name = "date")
+    public Long date;
+
+    @Column(name = "creator")
+    private String creator;
+
+    /**
+     * 头像
+     */
+    @Column(name = "profile_picture")
+    private String profilePicture;
+
+}
---
+++ src/main/java/com/dct/model/vo/ViewRouterMetaVO.java
@@ -0,0 +1,12 @@
+package com.dct.model.vo;
+
+import lombok.Data;
+
+/**
+ * @author Vic on 2019/1/8
+ */
+@Data
+public class ViewRouterMetaVO {
+    private String title;
+    private String icon;
+}
